8cd552b08e3e547fccb5f0e2e0d2aa1841046c49
Ilya Antipenko <aivus@aivus.name>
2015-03-31 00:10:18 +0300

    Use then-redis. Save repos in redis

--- services/repos.js
+++ services/repos.js
@@ -1,22 +1,27 @@
 var github = require('./githubApiClient').baseGHApiClient;
 var Promise = require('bluebird');
-//var redis = require("redis");
-//var client = redis.createClient();
+var redis = require("then-redis");
+var client = redis.createClient();
 
 // Retrieve and return user repos if they doesn't exist
 // If they are exist - return from session
 function getUserRepos(req, forceUpdate) {
-    if (!req.session.repos || forceUpdate == true) {
-        return github.user_repos({
-            access_token: req.session.access_token
-        }).then(function(repos) {
-            req.session.repos = repos;
-            return Promise.resolve(repos);
-        });
-    } else {
-        console.log('from the session');
-        return Promise.resolve(req.session.repos);
-    }
+    return client.hexists('user:' + req.session.user.id, 'repos').then(function(exists) {
+        if (!exists || forceUpdate == true) {
+            return github.user_repos({
+                access_token: req.session.access_token
+            }).then(function(repos) {
+                return client.hset('user:' + req.session.user.id, 'repos', repos).then(function(result) {
+                    return Promise.resolve(repos);
+                });
+            });
+        } else {
+            console.log('from the redis');
+            return client.hget('user:' + req.session.user.id, 'repos').then(function(repos) {
+                return Promise.resolve(repos);
+            });
+        }
+    });
 }
 
 module.exports = {
