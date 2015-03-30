6870e36d0faa0f76e19886ae38e05d2bba1553f9
Ilya Antipenko <aivus@aivus.name>
2015-03-31 00:42:30 +0300

    Hotfix. JSON.parse session user before use it

--- services/repos.js
+++ services/repos.js
@@ -5,19 +5,20 @@ var client = require('../helpers/redisHelper').client;
 // Retrieve and return user repos if they doesn't exist
 // If they are exist - return from session
 function getUserRepos(req, forceUpdate) {
-    return client.hexists('user:' + req.session.user.id, 'repos').then(function(exists) {
+    var user = JSON.parse(req.session.user);
+    return client.hexists('user:' + user.id, 'repos').then(function(exists) {
         if (!exists || forceUpdate == true) {
             return github.user_repos({
                 access_token: req.session.access_token
             }).then(function(repos) {
-                return client.hset('user:' + req.session.user.id, 'repos', repos).then(function(result) {
+                return client.hset('user:' + user.id, 'repos', repos).then(function(result) {
                     console.log('retrieve repos from github');
                     return Promise.resolve(repos);
                 });
             });
         } else {
             console.log('retrieve repos from redis');
-            return client.hget('user:' + req.session.user.id, 'repos').then(function(repos) {
+            return client.hget('user:' + user.id, 'repos').then(function(repos) {
                 return Promise.resolve(repos);
             });
         }
